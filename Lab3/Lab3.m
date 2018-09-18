6db78280621489d09d4e1d0ef1b86abddbef34f9
danenigma <danenigma@users.noreply.github.com>
2018-09-17 23:19:46 -0400

    Lab 3 still not working

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -1,91 +1,78 @@
-%% CASE I
-% lineObject instances represent bodies.
-walls = lineObject();
-% Specify points as an n x 2 array.
-% Simulator length units: m.
-walls.lines = [1.5 0; 3 1.5; 1.5 3; 0 1.5; 1.5 0];
-obstacle = lineObject();
-obstacle.lines = [1.6 1.4; 1.6 1.6; 1.4 1.6; 1.4 1.4; 1.6 1.4];
+close all; clear; clc;
 
-% Environments are specified as lineMap instances.
-% lineMap constructor takes an array of lineObject instances as input.
-map = lineMap([walls obstacle]);
-% Show the map.
-hf = map.plot();
+robot = raspbot();
 
-%% Fire simulator.
-% Close map plot.
-if ishandle(hf)
-    close(hf);
+
+x = 0; y= 0; th = 0;
+W = 0.084;
+
+thArray(1) = th;
+xArray(1) = x;
+yArray(1) = y;
+
+ks = 3.;
+kk = 15.1084;
+sf = 1.;
+v  = 0.2;
+tf = sf/v;
+kth = (2*pi)/sf;
+
+Tf = 2*tf;
+
+myPlot = plot(xArray, yArray, 'b-');
+xlim([-.50 .50]);
+ylim([-.50 .50]);
+
+
+sl_bias   = robot.encoders.LatestMessage.Vector.X;
+sr_bias   = robot.encoders.LatestMessage.Vector.Y;
+
+sl = 0;
+sr = 0;
+
+t  = 0;
+t_prev = 0;
+
+a = tic;
+
+while( t < Tf)
+    
+    t = toc(a)/ks;
+    dt = t - t_prev;
+    t_prev = t;
+    
+    s = v*t;
+    k = (kk/ks)*sin(kth*s);
+    w = k*v;
+    
+    VR = v + (W/2)*w;    
+    VL = v - (W/2)*w;
+
+    robot.sendVelocity(VL, VR)
+        
+
+    dsl = robot.encoders.LatestMessage.Vector.X - sl - sl_bias;
+    dsr = robot.encoders.LatestMessage.Vector.Y - sr - sr_bias;
+    
+    sl  = sl + dsl;
+    sr  = sr + dsr;
+    
+    
+    a_vl = dsl/dt;
+    a_vr = dsr/dt;
+    a_v  = (a_vl + a_vr)/2;
+    a_w  = (a_vr - a_vl)/W;
+
+    th =  th + a_w*dt;    
+    x  =  x  + cos(th)*a_v*dt;
+    y  =  y  + sin(th)*a_v*dt;
+     
+
+    pause(0.001);
+        
+    set(myPlot, 'xdata', [get(myPlot,'xdata') x],...
+    'ydata', [get(myPlot,'ydata') y]);
+        
 end
-
-% By default robot starts at pose x = 0, y = 0, theta = 0.
-%rob = raspbot('sim');
-% Start at desired pose.
-robot = raspbot('sim',[1; 1.5; 0]);
-%% Add map to simulator.
-% neato.genMap takes an array of lineObject instances as input. 
-robot.genMap(map.objects);
-%%
-%robot =  raspbot();
-
-%robot.encoders.NewMessageFcn = @encoderEventListener;
-previousLeftEncoder  = robot.encoders.LatestMessage.Vector.X;
-previousRightEncoder = robot.encoders.LatestMessage.Vector.Y;
-tic;
-tickCounter = 1;
-time = 0;
-timeArray = [];
-leftArray = [];
-cmdV = .050; 
-while 1
-    
-    robot.sendVelocity(cmdV, cmdV)
-    pause(.05);
-    
-    leftEncoder  = robot.encoders.LatestMessage.Vector.X;
-    rightEncoder = robot.encoders.LatestMessage.Vector.Y;
-    dT  = toc;
-    
-    dL = leftEncoder  - previousLeftEncoder;
-    dR = rightEncoder - previousRightEncoder;
-    
-    previousLeftEncoder  = leftEncoder;
-    previousRightEncoder = rightEncoder;
-    
-    time = time + dT
-    V   = 0.5*(dL+dR)/dT;
-    
-    timeArray(tickCounter) = time;
-    velocityArray(tickCounter) = V;
-    
-    tickCounter = tickCounter + 1;
-    if time > 2
-        robot.stop()
-        break
-    end
-    tic
-    
-end 
-figure;
-plot(timeArray, velocityArray);
-xlabel('Time (sec)')
-ylabel('Velocity m/sec')
-title('Velocity vs Time');
- %% CASE II
-robot =  raspbot();
-
-%robot.encoders.NewMessageFcn = @encoderEventListener;
-tic;
-
-while 1
-    
-    dL  = robot.encoders.LatestMessage.Vector.X;
-    dR  = robot.encoders.LatestMessage.Vector.Y;
-    dT  = toc;
-    V   = 0.5*(dL+dR)/dT; 
-    tic
-    pause(0.005);
-
-end 
-          
+pause(0.01)
+robot.stop()
